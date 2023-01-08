6c3d333f3af1c51bc5cf81b53484a1f210644d09
mehmet-akif-tanisik <matnsk@hotmail.com>
2023-01-08 22:30:09 +0300

    deleted all

--- emlakcepte-logger-service/src/main/java/com/emlakcepteloggerservice/repository/LogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.emlakcepteloggerservice.repository;
-
-import com.emlakcepteloggerservice.model.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Integer> {
-
-}
